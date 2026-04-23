75bfcb97378f47e4da292f4ba70ba0d35ef44cb6
maruf-git <munna.csehstu@gmail.com>
2026-04-23 16:14:46 +0600

    update layout, homepage, alert details modal, navbar, admin login page

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,14 +5,17 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "11th Hour Disaster Alert",
+  title: "Real-time Disaster Alert System",
   description: "Real-time automated disaster monitoring system",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className} bg-slate-50 text-slate-900 antialiased`} suppressHydrationWarning>
+      <body
+        className={`${inter.className} bg-slate-50 text-slate-900 antialiased`}
+        suppressHydrationWarning
+      >
         <Navbar />
         {children}
       </body>
